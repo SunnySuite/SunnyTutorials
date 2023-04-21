22776f3eb93a24d91d34b1af11cdbda035f83c4e
ddahlbom <david.dahlbom@gmail.com>
2023-04-21 19:13:11 -0400

    Updated make.jl script

--- make.jl
+++ make.jl
@@ -3,13 +3,21 @@ using Literate, Dates, Git
 root = dirname(@__FILE__)
 example_dir = joinpath(root, "Sunny.jl", "examples", "longer_examples")
 save_dir = joinpath(root, "Tutorials")
-tutorials = filter(name -> split(name, ".")[end] == "jl", readdir(example_dir))
 
+# If no arguments given, rebuild all notebooks
+if length(ARGS) == 1
+    tutorials = filter(name -> split(name, ".")[end] == "jl", readdir(example_dir))
+else
+    tutorials = ARGS[2:end]
+end
+
+# Build the notebooks
 map(tutorials) do tutorial
     Literate.notebook(joinpath(example_dir, tutorial), save_dir; execute=true)
 end
 
-date = string(Dates.now())
+# Sync with github
+run(`$(git()) pull`) # Make sure up to date
 run(git(["add", save_dir*"/*.ipynb"]))
-run(`$(git()) commit -am "Auto-generated tutorials $date"`)
+run(`$(git()) commit -am "Auto-generated tutorials $(string(Dates.now()))"`)
 run(`$(git()) push`)
