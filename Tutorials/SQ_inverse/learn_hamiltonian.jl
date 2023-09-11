1d0fe92c54de1ba2e18998ac784a518dac4ed195
Sam Quinn <sq@gatech.edu>
2023-09-11 14:12:34 -0400

    Update & add pre-rendered data

--- Tutorials/SQ_inverse/learn_hamiltonian.jl
+++ Tutorials/SQ_inverse/learn_hamiltonian.jl
@@ -1,4 +1,4 @@
-using Sunny, LinearAlgebra, GLMakie
+using Sunny, LinearAlgebra, GLMakie, Optim
 
 # Load experiment data (so we have histogram_parameters)
 histogram_parameters, data = load_nxs("experiment_data_normalized.nxs")
@@ -25,17 +25,15 @@ energy_multisample = [(n + 0.5)/5 for n = 1:5]
 J2 = 0.16
 set_exchange!(sys,J2,Bond(1,1,[1,1,0]))
 
-# Unknown J1, D
-function forward_problem(J1,D)
+# Unknown J1, A
+function forward_problem(J1,A)
   set_exchange!(sys,J1,Bond(1,1,[1,0,0]))
   
   Sz = spin_operators(sys,1)[3]
-  set_onsite_coupling!(sys,D*Sz^2,1)
+  set_onsite_coupling!(sys,A*Sz^2,1)
 
   # Standard calculation:
   randomize_spins!(sys)
-  minimize_energy!(sys)
-  minimize_energy!(sys)
   minimize_energy!(sys)
 
   swt = SpinWaveTheory(sys)
@@ -56,12 +54,6 @@ function forward_problem(J1,D)
   return intensity ./ counts
 end
 
-J1_guess = 11.3
-A_guess = 0.03
-intensities_guess = forward_problem(J1_guess, A_guess)
-loss = loss_function(data, intensities_guess)
-
-
 function loss_function(experiment_data,simulation_data)
   Z_experiment = get_Z(experiment_data)
   normalized_exp_data = experiment_data ./ Z_experiment
@@ -78,7 +70,58 @@ function loss_function(experiment_data,simulation_data)
   sqrt(sum(weights .* squared_errors))
 end
 
-# Plot the data
-#using GLMakie
+function get_loss(parameters)
+  J1,A = parameters
+  simulation_data = forward_problem(J1,A)
+  return loss_function(data, simulation_data)
+end
 
+# Parameter sweep to generate loss landscape
+do_sweep = false
+Js, As, loss_landscape = if !do_sweep
+  include("pre_render_landscape.jl")
+  Js, As, loss_landscape
+else
+  nJ = 30
+  nA = 30
+  loss_landscape = zeros(Float64,nJ,nA)
+  Js = range(9.6,10.6,length=nJ)
+  As = range(0.06,0.09,length=nA)
+  for (ij,J) in enumerate(Js)
+    for (id,A) in enumerate(As)
+      @time loss_landscape[ij,id] = get_loss([J,A])
+    end
+  end
+  Js, As, loss_landscape
+end
 
+println("Got landscape")
+
+fig = Figure()
+ax = Axis(fig[1,1],xlabel = "J [meV]", ylabel = "A [meV]")
+heatmap!(ax,Js,As,loss_landscape)
+
+# Example use of Optim.jl to find minimum iteratively
+#=
+x0 = [10.6,0.075]
+opt_result = optimize(get_loss
+                     ,x0
+                     ,method=GradientDescent(alphaguess=1e-3)
+                     ,store_trace=true
+                     ,extended_trace = true
+                     ,time_limit=120.)
+
+lines!(ax,Point2f.(Optim.x_trace(opt_result)))
+scatter!(ax,-1,10)
+=#
+display(fig)
+
+# Plot plausible model result
+is = forward_problem(10.1,0.075)
+is ./= get_Z(is)
+is[isnan.(data)] .= NaN
+
+fig = Figure()
+heatmap!(Axis(fig[1,1]),sum(is[12:14,:,1,3:end],dims=1)[1,:,:])
+heatmap!(Axis(fig[1,2]),sum(data[12:14,:,1,3:end],dims=1)[1,:,:] ./ get_Z(data))
+display(fig)
